b59a7c13e56b86724299fa8893226b9b2f7d559c
Nikolai Varlamov <varlamow.col@yahoo.com>
2023-07-19 01:13:22 +0300

    [github-runner] github runner manifest fix

--- github-runner/delete.sh
+++ github-runner/delete.sh
@@ -1,3 +1,4 @@
-export TYPE=${TYPE:-"organization"} # ra
+export TYPE=${TYPE:-"organization"} # repository/organization/enterprise
+export SOURCE=${SOURCE:-"organization-name"} # your repo / org / enterprise name
 
 for f in *.yaml; do envsubst < $f | kubectl delete -f -; done
