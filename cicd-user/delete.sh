6620cefbb8208f016b4a6909b682e02119e64ab0
Nikolai Varlamov <varlamow.col@yahoo.com>
2023-08-02 10:57:54 +0300

    [cicd] token added

--- cicd-user/delete.sh
+++ cicd-user/delete.sh
@@ -1,3 +1,5 @@
+kubectl delete token calcoulomb -n calcoulomb
+
 kubectl delete -f 01-rolebinding.yaml
 
 kubectl delete serviceaccount calcoulomb -n calcoulomb
--- cicd-user/run.sh
+++ cicd-user/run.sh
@@ -4,6 +4,8 @@ kubectl create serviceaccount calcoulomb -n calcoulomb
 
 kubectl apply -f 01-rolebinding.yaml
 
+kubectl create token calcoulomb -n calcoulomb
+
 SECRET_NAME=$(kubectl get serviceaccount calcoulomb -n calcoulomb -o jsonpath='{.secrets[0].name}')
 TOKEN=$(kubectl get secret $SECRET_NAME -n calcoulomb -o jsonpath='{.data.token}' | base64 --decode)
 
