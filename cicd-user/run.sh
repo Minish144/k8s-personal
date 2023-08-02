21eed5bbcc054881fd05af0fed6b43ffad0d81e5
Nikolai Varlamov <varlamow.col@yahoo.com>
2023-08-02 10:31:44 +0300

    [cicd] added cicd set up

--- cicd-user/run.sh
+++ cicd-user/run.sh
@@ -4,7 +4,7 @@ kubectl create serviceaccount calcoulomb -n calcoulomb
 
 kubectl apply -f 01-rolebinding.yaml
 
-SECRET_NAME=$(kubectl get serviceaccount your-serviceaccount -n your-namespace -o jsonpath='{.secrets[0].name}')
-TOKEN=$(kubectl get secret $SECRET_NAME -n your-namespace -o jsonpath='{.data.token}' | base64 --decode)
+SECRET_NAME=$(kubectl get serviceaccount calcoulomb -n calcoulomb -o jsonpath='{.secrets[0].name}')
+TOKEN=$(kubectl get secret $SECRET_NAME -n calcoulomb -o jsonpath='{.data.token}' | base64 --decode)
 
 echo "${TOKEN}"
